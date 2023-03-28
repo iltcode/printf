6ba7a30d9f22764afa5b4d03c7ed62121ca8f260
iltcode <iltcodep@gmail.com>
2023-03-28 15:54:06 -0700

    _printf is updated

--- main.h
+++ main.h
@@ -46,6 +46,7 @@ va_list list, char buffer[], int flags, int width, int precision, int size);
 /****************** FUNCTIONS ******************/
 
 /* Funtions to print chars and strings */
+int _putchar(char c);
 int print_char(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_string(va_list types, char buffer[],
